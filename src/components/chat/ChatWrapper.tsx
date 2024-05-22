df881f98d4cc44b5b72a5f7182ecd7fb036cc4d3
Sahil <sahiltambe81922@gmail.com>
2024-05-22 08:19:39 +0530

    processing / failed changes

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -7,23 +7,23 @@ import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 
 
-interface props{
-  fileId:string
+interface props {
+  fileId: string
 }
 
 
-export default function ChatWrapper({fileId}:props) {
+export default function ChatWrapper({ fileId }: props) {
 
-  const {data, isLoading} = trpc.getFileStatus.useQuery(
+  const { data, isLoading } = trpc.getFileStatus.useQuery(
     {
-      fileId:fileId
-    },{
-      refetchInterval:({state:{data}})=>{
-        //console.log(data)
-        return data?.status==='SUCCESS' || data?.status==='FAILED' ? false : 500;
-        // return ='SUCCESS' || data.state.status=='FAILED' ? false : 500
-      }
+      fileId: fileId
+    }, {
+    refetchInterval: ({ state: { data } }) => {
+      //console.log(data)
+      return data?.status === 'SUCCESS' || data?.status === 'FAILED' ? false : 500;
+      // return ='SUCCESS' || data.state.status=='FAILED' ? false : 500
     }
+  }
   )
 
   if (isLoading)
@@ -45,65 +45,67 @@ export default function ChatWrapper({fileId}:props) {
       </div>
     )
 
-    if (data?.status === 'PROCESSING')
-      return (
-        <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
-          <div className='flex-1 flex justify-center items-center flex-col mb-28'>
-            <div className='flex flex-col items-center gap-2'>
-              <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
-              <h3 className='font-semibold text-xl'>
-                Processing PDF...
-              </h3>
-              <p className='text-zinc-500 text-sm'>
-                This won&apos;t take long.
-              </p>
-            </div>
+  if (data?.status !== 'SUCCESS')
+    return (
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+          <div className='flex flex-col items-center gap-2'>
+            {
+              data?.status === 'FAILED' ?
+                (
+                  <>
+                    <XCircle className='h-8 w-8 text-red-500' />
+                    <h3 className='font-semibold text-xl'>
+                      Too many pages in PDF
+                    </h3>
+                    <p className='text-zinc-500 text-sm'>
+                      Your{' '}
+                      <span className='font-medium'>
+                        {'Free'}
+                      </span>{' '}
+                      plan supports up to{' '}
+                      5
+                      pages per PDF.
+                    </p>
+                    <Link
+                      href='/dashboard'
+                      className={buttonVariants({
+                        variant: 'secondary',
+                        className: 'mt-4',
+                      })}>
+                      <ChevronLeft className='h-3 w-3 mr-1.5' />
+                      Back
+                    </Link>
+                  </>
+                ) :
+                (
+                  <>
+                    <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
+                    <h3 className='font-semibold text-xl'>
+                      Processing PDF...
+                    </h3>
+                    <p className='text-zinc-500 text-sm'>
+                      This won&apos;t take long.
+                    </p>
+                  </>
+                )
+            }
+
           </div>
-  
-          <ChatInput isDisabled />
         </div>
-      )
+
+        <ChatInput isDisabled />
+      </div >
+    )
+
   
-    if (data?.status === 'FAILED')
-      return (
-        <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
-          <div className='flex-1 flex justify-center items-center flex-col mb-28'>
-            <div className='flex flex-col items-center gap-2'>
-              <XCircle className='h-8 w-8 text-red-500' />
-              <h3 className='font-semibold text-xl'>
-                Too many pages in PDF
-              </h3>
-              <p className='text-zinc-500 text-sm'>
-                Your{' '}
-                <span className='font-medium'>
-                  { 'Free'}
-                </span>{' '}
-                plan supports up to{' '}
-                5
-                pages per PDF.
-              </p>
-              <Link
-                href='/dashboard'
-                className={buttonVariants({
-                  variant: 'secondary',
-                  className: 'mt-4',
-                })}>
-                <ChevronLeft className='h-3 w-3 mr-1.5' />
-                Back
-              </Link>
-            </div>
-          </div>
-  
-          <ChatInput isDisabled />
-        </div>
-      )
 
   return (
     <div className="relative min-h-full bg-zinc-50 flex divide-zinc-200">
       <div className="flex flex-col flex-1 justify-between mb-28">
-        <Message/>
+        <Message />
       </div>
-      <ChatInput isDisabled={false}/>
+      <ChatInput isDisabled={false} />
     </div>
   )
 }
